39b20dd8023c44fc92d3ff71c9b191ff2e51e9be
sumant1512 <sumantmishra511@gmail.com>
2024-12-21 18:09:32 +0530

    basic hader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'seetimaar-films';
-}
+export class AppComponent {}
