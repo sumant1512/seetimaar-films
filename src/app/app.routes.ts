e665223945a99bb1f60c50f34aabe9bdcdc8d011
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-21 23:22:45 +0530

    mobile header icon issue fixed (#8) (#9)
    pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./contact-us/contact-us.component').then(
+        (m) => m.ContactUsComponent
+      ),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
