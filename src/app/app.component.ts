11831d85b8c6c3a0ec57c781a3a9fce4d488c9f6
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-22 01:02:41 +0530

    videos added (#10)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/video/video.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-video',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './video.component.html',
+  styleUrl: './video.component.scss',
+})
+export class VideoComponent implements AfterViewInit {
+  @Input() videoSrc!: string;
+  @Input() width: string = '600px';
+  @Input() height = '400px';
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef;
+
+  ngAfterViewInit() {
+    this.autoPlayVideo();
+  }
+
+  autoPlayVideo() {
+    const videoElement = this.videoPlayer.nativeElement;
+
+    videoElement.muted = true;
+    videoElement.autoplay = true;
+    videoElement.loop = true;
+
+    videoElement
+      .play()
+      .then(() => {
+        console.log('Video is playing successfully.');
+      })
+      .catch((error: any) => {
+        console.error('Error attempting to play the video:', error);
+      });
+  }
+}
