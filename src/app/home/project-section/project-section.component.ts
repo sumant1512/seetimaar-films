a39b3360365cf12f8c58abf11d67e0076d6ed647
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-24 03:10:34 +0530

    project structure updated (#14)

---
+++ src/app/home/project-section/project-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-section.component.html',
+  styleUrl: './project-section.component.scss'
+})
+export class ProjectSectionComponent {
+
+}
