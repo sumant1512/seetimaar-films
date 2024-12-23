0e9da7e738a49e7c356822e9a82bda9ba1fa575e
sumant1512 <sumantmishra511@gmail.com>
2024-12-24 03:10:03 +0530

    project structure updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { ProjectSectionComponent } from './project-section/project-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AboutSectionComponent, ProjectSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
