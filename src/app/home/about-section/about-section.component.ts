e3a5635ed4bf4af7e6c8b5971604b87508b16d3f
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-26 03:16:50 +0530

    home p1 done (#22)

--- src/app/home/about-section/about-section.component.ts
+++ src/app/home/about-section/about-section.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about-section',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './about-section.component.html',
-  styleUrl: './about-section.component.scss'
+  styleUrl: './about-section.component.scss',
 })
-export class AboutSectionComponent {
-
-}
+export class AboutSectionComponent {}
