369faa6966d5df2db8dc295be6cda4c33e850638
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-26 03:01:55 +0530

    availability section added (#21)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { AboutSectionComponent } from './about-section/about-section.component';
 import { ProjectSectionComponent } from './project-section/project-section.component';
+import { ConsultationComponent } from './consultation/consultation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutSectionComponent, ProjectSectionComponent],
+  imports: [
+    AboutSectionComponent,
+    ProjectSectionComponent,
+    ConsultationComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
