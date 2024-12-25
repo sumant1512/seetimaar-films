8d47d65af52f6ed98e24bed43585ec47cb25bef0
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-26 01:18:35 +0530

    footer added (#18)

--- src/app/home/project-section/project-section.component.ts
+++ src/app/home/project-section/project-section.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-project-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './project-section.component.html',
-  styleUrl: './project-section.component.scss'
+  styleUrl: './project-section.component.scss',
 })
 export class ProjectSectionComponent {
-
+  projects = [
+    {
+      id: 1,
+      name: 'The Story of Guns Valley',
+      description:
+        'A gripping tale of crime, survival, and redemption, exploring the human stories behind the violence in a lawless valley',
+      img: 'project1.png',
+    },
+    {
+      id: 2,
+      name: "Veena's journey",
+      description:
+        'After a devastating love marriage collapse, a young woman from Urban place, lost in despair, encounters a mysterious saint who offers her hope',
+      img: 'project2.png',
+    },
+  ];
 }
