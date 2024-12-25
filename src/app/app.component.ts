508963003a774fcb5207bbef4789d04dee752e0f
Sumant Mishra <45934429+sumant1512@users.noreply.github.com>
2024-12-26 00:37:22 +0530

    footer added (#16)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderNewComponent } from './header-new/header-new.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderNewComponent, RouterModule, CommonModule],
+  imports: [HeaderNewComponent, FooterComponent, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
