7b6616f72d391b4ad4fb8020225da831e98d83ec
sumant1512 <sumantmishra511@gmail.com>
2024-12-28 01:11:31 +0530

    project page added

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  tabs = ['All', 'Corporate', 'Private', 'Commercial', 'TV & Films'];
+  selectedTab = this.tabs[0];
+  projects = [
+    {
+      id: 1,
+      name: 'The Story of Guns Valley',
+      description:
+        'A gripping tale of crime, survival, and redemption, exploring the human stories behind the violence in a lawless valley.',
+      img: 'p1.png',
+      isLatest: true,
+    },
+    {
+      id: 2,
+      name: "Veena's journey",
+      description:
+        'After a devastating love marriage collapse, a young woman from Urban place, lost in despair, encounters a mysterious saint who offers her hope.',
+      img: 'p1.png',
+    },
+    {
+      id: 3,
+      name: 'The Story of Guns Valley',
+      description:
+        'A gripping tale of crime, survival, and redemption, exploring the human stories behind the violence in a lawless valley.',
+      img: 'p1.png',
+    },
+    {
+      id: 4,
+      name: "Veena's journey",
+      description:
+        'After a devastating love marriage collapse, a young woman from Urban place, lost in despair, encounters a mysterious saint who offers her hope.',
+      img: 'p1.png',
+    },
+    {
+      id: 5,
+      name: 'The Story of Guns Valley',
+      description:
+        'A gripping tale of crime, survival, and redemption, exploring the human stories behind the violence in a lawless valley.',
+      img: 'p1.png',
+    },
+    {
+      id: 6,
+      name: "Veena's journey",
+      description:
+        'After a devastating love marriage collapse, a young woman from Urban place, lost in despair, encounters a mysterious saint who offers her hope.',
+      img: 'p1.png',
+    },
+  ];
 
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
 }
