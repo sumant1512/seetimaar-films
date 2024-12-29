2e38548648d6e1319e1c3f6760429bd9f814af60
sumant1512 <sumantmishra511@gmail.com>
2024-12-29 19:12:20 +0530

    contact us page added

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { VideoComponent } from '../+shared/video/video.component';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [VideoComponent],
+  imports: [CommonModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  contactDetails = [
+    {
+      label: 'Call Us',
+      value: '+91 9993035739',
+      icon: 'phone.png',
+      href: 'tel:+91 9993035739',
+    },
+    {
+      label: 'Mail Us',
+      value: 'chitransh@seetimaarfilms.com',
+      icon: 'email.png',
+      href: 'mailto:chitransh@seetimaarfilms.com',
+    },
+    {
+      label: 'Social Media',
+      icon: 'social-media.png',
+    },
+  ];
+
+  socialMedia = [
+    { icon: 'facebook.png', path: '' },
+    { icon: 'whatsapp.png', path: '' },
+    { icon: 'instagram.png', path: '' },
+    { icon: 'linkdin.png', path: '' },
+  ];
+}
