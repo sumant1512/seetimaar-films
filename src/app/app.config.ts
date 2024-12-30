cda82667f960002ddd5d61f7aee51ce27f88879b
sumant1512 <sumantmishra511@gmail.com>
2024-12-30 10:03:29 +0530

    spinner added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
@@ -8,8 +12,26 @@ import {
 
 import { routes } from './app.routes';
 
+export function appInitializer(): () => Promise<void> {
+  return () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        const loader = document.getElementById('global-loader');
+        if (loader) {
+          loader.classList.add('hidden');
+        }
+        resolve();
+      }, 1000);
+    });
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializer,
+      multi: true,
+    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
