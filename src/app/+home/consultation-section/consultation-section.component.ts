cdffa1af7e0c91de761843057a6879c2208a2d64
sumant1512 <sumantmishra511@gmail.com>
2025-01-03 21:02:31 +0530

    changes added for email

--- src/app/+home/consultation-section/consultation-section.component.ts
+++ src/app/+home/consultation-section/consultation-section.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-consultation-section',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './consultation-section.component.html',
   styleUrl: './consultation-section.component.scss',
 })
-export class ConsultationSectionComponent {}
+export class ConsultationSectionComponent {
+  serviceID = 'service_5vbppd5';
+  templateID = 'template_q1tnor7';
+  publicKey = 'SxqZlqYGgxgAgw6he';
+  consultationForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl(''),
+    phone: new FormControl('', Validators.required),
+    purpose: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  sendEmail() {
+    console.log('called send email', this.consultationForm.value);
+    emailjs
+      .send(
+        this.serviceID,
+        this.templateID,
+        this.consultationForm.value,
+        this.publicKey
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log('Email sent successfully:', result.text);
+        },
+        (error: any) => {
+          console.error('Error sending email:', error.text);
+        }
+      );
+  }
+}
